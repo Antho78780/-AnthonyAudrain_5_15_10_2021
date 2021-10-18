d348e687c44c181e851f065f09473750131e162a
Anthony Audrain <anthonyaud78@gmail.com>
2021-10-18 16:30:07 +0200

    fait

--- front/js/script.js
+++ front/js/script.js
@@ -1,4 +1,36 @@
 
+function kanap (){
+    fetch("http://localhost:3000/api/products")
+    .then(response => response.json())
+    .then(data => {
+        document.getElementById("items")
+        console.log(items)
+    })
+}
+kanap();  
+
+       
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
