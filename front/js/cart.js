a085a5b7e3f50a386db9e720e7e4b94c1d125b7b
antho <anthonyaud78@gmail.com>
2021-11-16 16:17:22 +0100

    SAVE

--- front/js/cart.js
+++ front/js/cart.js
@@ -7,13 +7,14 @@ const arrayPrice = [];
 const arrayQuantity = [];
 
 ///////////// AJOUT DE TOUTE LES OPTIONS DU PRODUIT DANS UNE BOUCLE //////////////////
-recupLocalStorage.forEach(panier => {
+
+for (let panier of recupLocalStorage) {
     const quantite = parseInt(panier.quantite);
     const total = panier.price * quantite;
     arrayQuantity.push(quantite);
     arrayPrice.push(total);
-
-    document.querySelector("#cart__items").innerHTML += 
+    
+    document.querySelector("#cart__items").innerHTML +=
     `<article class="cart__item"data-id="${panier.id}"><div class="cart__item__img"><img src="${panier.img}" alt="${panier.altTxt}"></div><div class="cart__item__content">
     <div class="cart__item__content__titlePrice"><h2>${panier.name}</h2><p>${total} €</p></div><div class="cart__item__content__settings">
     <div class="cart__item__content__settings__quantity"><p>Quantité : </p><input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantite}">
@@ -25,48 +26,58 @@ recupLocalStorage.forEach(panier => {
         event.preventDefault();
         alert( "Vous avez supprimé l'article " + panier.name + " du panier");
     })
-}); 
+}
+console.log(recupLocalStorage)
 const totalPrice = arrayPrice.reduce(additionPrixEtQuantite);
 const totalQuantite = arrayQuantity.reduce(additionPrixEtQuantite);
 
+
 document.querySelector("#totalPrice").innerHTML = `${totalPrice}`;
 document.querySelector("#totalQuantity").innerHTML = `${totalQuantite}`;
-console.log(recupLocalStorage);
 
 const recupFormulaire = document.querySelector(".cart__order__form");
-const recupOrderId = document.querySelector("#orderId");
 const envoyerCommande = document.querySelector("#order");
 
 
-const products = [];
+    envoyerCommande.addEventListener("click", function(event) {
+        event.preventDefault();
+    
+        const contact = {
+            firstName: recupFormulaire[0].value,
+            lastName: recupFormulaire[1].value,
+            address: recupFormulaire[2].value,
+            city: recupFormulaire[3].value,
+            email: recupFormulaire[4].value,
+        }
+        const products = []; 
 
-for (let recupLocalStorageId of recupLocalStorage) {
-    products.push(recupLocalStorageId.id)
-}
-envoyerCommande.addEventListener("click", function(event) {
-    event.preventDefault();
-    const contact = {
-        firstName: recupFormulaire[0],
-        lastName: recupFormulaire[1],
-        address: recupFormulaire[2],
-        city: recupFormulaire[3],
-        email: recupFormulaire[4],
-        products,
-    } 
-    console.log(contact)
-})
+        for (let recupId of recupLocalStorage) {
+            products.push(recupId.id);
+        }
+        const obj = {
+            contact,
+            products,
+        }
+
+        console.log(obj)
+
+        const requestPost = fetch("http://localhost:3000/api/products/order", {
+            method: "POST",
+            body: JSON.stringify(obj),
+            headers: {
+                'Content-Type': 'application/json',
+                'Accept': 'application/json',
+            },
+        })
+        console.log(requestPost);
+        requestPost.then (reponse => reponse.json())
+        .then (data => {
+            console.log(data);
+        })
+    })
 
 
-const requestPost = fetch("http://localhost:3000/api/products/order", {
-    method: "POST",
-    headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(envoyerCommande),
-})
-
-
+   
 
 
 
