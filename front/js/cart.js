219101bb593700d9604c9cdb05b2f3ca3dff42d7
antho <anthonyaud78@gmail.com>
2021-11-24 10:53:15 +0100

    save

--- front/js/cart.js
+++ front/js/cart.js
@@ -9,30 +9,32 @@ const arrayPrice = [];
 const arrayQuantity = [];
 
     ///////////// AJOUT DE TOUTE LES OPTIONS DU PRODUIT DANS UNE BOUCLE //////////////////
-recupLocalStorage.forEach(panier => {
-    let quantite = parseInt(panier.quantite);
-    const total = panier.price * quantite;
+for (let i = 0;i<recupLocalStorage.length;i++) {
+    let quantite = parseInt(recupLocalStorage[i].quantite);
+    const total = recupLocalStorage[i].price * quantite;
     arrayQuantity.push(quantite);
     arrayPrice.push(total);
     const recupArticle = document.querySelector("#cart__items");
     if(recupArticle) {
-        recupArticle.innerHTML +=`<article class="cart__item"data-id="${panier.id}"><div class="cart__item__img"><img src="${panier.img}" alt="${panier.altTxt}">
-        </div><div class="cart__item__content"><div class="cart__item__content__titlePrice"><h2>${panier.name}</h2><p>${total} €</p></div><div class="cart__item__content__settings">
+        recupArticle.innerHTML +=`<article class="cart__item"data-id="${recupLocalStorage[i].id}"><div class="cart__item__img"><img src="${recupLocalStorage[i].img}" alt="${recupLocalStorage[i].altTxt}">
+        </div><div class="cart__item__content"><div class="cart__item__content__titlePrice"><h2>${recupLocalStorage[i].name}</h2><p>${total} €</p></div><div class="cart__item__content__settings">
         <div class="cart__item__content__settings__quantity"><p>Quantité : </p><input type="number" class="itemQuantity" name="itemQuantity" min="1" max="100" value="${quantite}">
         </div><div class="cart__item__content__settings__delete"><p class="deleteItem">Supprimer</p>
-        </div></div></div></article`;  
+        </div></div></div></article`;
+        console.log(recupLocalStorage[i]);
         const recupItemQuantity = document.querySelectorAll(".itemQuantity");
         for (let i = 0; i < recupItemQuantity.length;i++) {
             recupItemQuantity[i].addEventListener("click", function(e) {
                 e.preventDefault();
+                console.log(recupLocalStorage[i])
                 const valueQuantite = recupItemQuantity[i].value;
-                console.log(valueQuantite)
+                console.log(valueQuantite);
                 if(valueQuantite) {
-                    quantite++;
-                    panier.quantite = quantite;
+                    quantite = valueQuantite;
+                    recupLocalStorage[i].quantite = quantite;
+                    localStorage.setItem("panier", JSON.stringify(recupLocalStorage));
                 }else {
                 }
-                
             })
         }
     }else {
@@ -49,7 +51,7 @@ recupLocalStorage.forEach(panier => {
         recupQuantite.innerHTML = `${totalQuantite}`
     }else {
     }
-});
+}
 const deleteItem = document.querySelectorAll(".deleteItem");
 for (let i=0; i<deleteItem.length;i++) {
     deleteItem[i].addEventListener("click", function(e) {
@@ -64,98 +66,96 @@ for (let i=0; i<deleteItem.length;i++) {
 }
 const recupFormulaire = document.querySelector(".cart__order__form");
 const envoyerCommande = document.querySelector("#order");
-     
-if(envoyerCommande) {
-    envoyerCommande.addEventListener("click", function(event) {
-        event.preventDefault();
-        const contact = {
-            firstName: recupFormulaire[0].value,
-            lastName: recupFormulaire[1].value,
-            address: recupFormulaire[2].value,
-            city: recupFormulaire[3].value,
-            email: recupFormulaire[4].value,
+const recupOrderId = document.querySelector("#orderId");
+        if(envoyerCommande) {
+            envoyerCommande.addEventListener("click", function(event) {
+                event.preventDefault();
+                const contact = {
+                    firstName: recupFormulaire[0].value,
+                    lastName: recupFormulaire[1].value,
+                    address: recupFormulaire[2].value,
+                    city: recupFormulaire[3].value,
+                    email: recupFormulaire[4].value,
+                }
+                const products = []; 
+                for (let recupId of recupLocalStorage) {
+                    products.push(recupId.id);
+                }
+                const objetContactEtProducts = {
+                    contact,
+                    products,
+                }
+                function prenom () {
+                    const regexPrenom = contact.firstName;
+                    if(/^[A-Za-z]{3,15}$/.test(regexPrenom)) {
+                        return true;
+                    }else {
+                        return false;
+                    };
+                };
+                function nom () {
+                    const regexNom = contact.lastName;
+                    if(/^[A-Za-z]{3,15}$/.test(regexNom)) {
+                        return true;
+                    }else {
+                    return false;
+                    };
+                }
+                function address () {
+                    const regexAdress = contact.address;
+                    if(/^[ A-Za-z-0-9 ]{5,25}$/.test(regexAdress)) {
+                        return true;
+                    }else {
+                        return false;
+                    };
+                };
+                function ville() {
+                    const regexVille = contact.city;
+                    if(/^[A-Za-z]{4,15}$/.test(regexVille)) {
+                        return true;
+                    }else {
+                    return false;
+                    };
+                }
+                function email () {
+                    const regexEmail = contact.email;
+                    if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(regexEmail)) {
+                        return true;
+                    }else {
+                        return false;
+                    };
+                };
+                    const requestPost = fetch(`http://localhost:3000/api/products/order`, {
+                        method: "POST",
+                        body : JSON.stringify(objetContactEtProducts),
+                        headers: {
+                            'Content-Type': 'application/json',
+                            'Accept': 'application/json',
+                        },
+                    })
+                        requestPost
+                        .then (res => res.json())    
+                        .then (data => {
+                            if(prenom() && nom() && address() && ville() & email()) {
+                            }
+                            else {
+                                alert("Informations incorrect")
+                                data = undefined;
+                                console.log("Le formulaire ne peut pas étre envoyer avec des mauvaises informations");
+                            }
+                            console.log("Réponse du back-end")
+                            console.log(data);
+                            console.log("OrderId")
+                            console.log(data.orderId);
+                            console.log(window.location.href);
+                            
+                        });
+                /*
+                window.location = "confirmation.html";
+                */
+            }) 
+        }else {
         }
-        const products = []; 
-        for (let recupId of recupLocalStorage) {
-            products.push(recupId.id);
-        }
-        const objetContactEtProducts = {
-            contact,
-            products,
-        }
-        function prenom () {
-            const regexPrenom = contact.firstName;
-            if(/^[A-Za-z]{3,15}$/.test(regexPrenom)) {
-                return true;
-            }else {
-                return false;
-            };
-        };
-        function nom () {
-            const regexNom = contact.lastName;
-            if(/^[A-Za-z]{3,15}$/.test(regexNom)) {
-                return true;
-            }else {
-            return false;
-            };
-        }
-        function address () {
-            const regexAdress = contact.address;
-            if(/^[ A-Za-z-0-9 ]{5,25}$/.test(regexAdress)) {
-                return true;
-            }else {
-                return false;
-            };
-        };
-        function ville() {
-            const regexVille = contact.city;
-            if(/^[A-Za-z]{4,15}$/.test(regexVille)) {
-                return true;
-            }else {
-            return false;
-            };
-        }
-        function email () {
-            const regexEmail = contact.email;
-            if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(regexEmail)) {
-                return true;
-            }else {
-                return false;
-            };
-        };
-        const requestPost = fetch(`http://localhost:3000/api/products/order`, {
-            method: "POST",
-            body : JSON.stringify(objetContactEtProducts),
-            headers: {
-                'Content-Type': 'application/json',
-                'Accept': 'application/json',
-            },
-        })
-        requestPost
-        .then (res => res.json())    
-        .then (data => {
-            if(prenom() && nom() && address() && ville() & email()) {
-            }
-            else {
-                alert("Informations incorrect")
-                data = undefined;
-                console.log("Le formulaire ne peut pas étre envoyer avec des mauvaises informations");
-            }
-            console.log("Réponse du back-end")
-            console.log(data);
-            console.log("OrderId")
-            console.log(data.orderId);
-        });
-        
-        /*
-        window.location = "confirmation.html";
-        */
-    }) 
-}else {
-    const recupOrderId = document.querySelector("#orderId");
-    console.log(recupOrderId);
-    recupOrderId.innerHTML = ``;
-}
     
    
    
