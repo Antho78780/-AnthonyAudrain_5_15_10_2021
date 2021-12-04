b2b38dc53e60b68ece678b7a84ce671afe985872
Anthony Audrain <anthonyaud78@gmail.com>
2021-12-04 19:36:14 +0100

    modification indentation

--- front/js/product.js
+++ front/js/product.js
@@ -18,7 +18,7 @@ requete
     ///// Ajout de la possibilité d'afficher les options de la couleur du produit gràce a une boucle/////////
     for (let color of product.colors) {
         document.querySelector("#colors").innerHTML += `<option value="${color}">${color}</option>`
-}
+    }
       //////////// Récupération de la constante sendPanier pour écouter le click ////////////
     sendPanier.addEventListener("click", function(event) {
         event.preventDefault();
@@ -34,44 +34,41 @@ requete
         
         //// création d'un tableau LocalStorage ////
         let array = [];
-        array = JSON.parse(localStorage.getItem("panier"));;
+        array = JSON.parse(localStorage.getItem("panier"));
        
         ////////// AJOUT DE CONDITIONS POUR STOCKER LESPRODUITS DANS LE LOCALSTORAGE//////////////
              
-             if(array && optionsProduit.colors != "" && optionsProduit.quantite != "0") {
+            if(array && optionsProduit.colors != "" && optionsProduit.quantite != "0") {
                 let produitExisteDeja = array.find(el => el.id == optionsProduit.id);
                 if(produitExisteDeja) {
-                    let resultQuantite = [];
-                    let resultQuantiteMoins = [];
+                    let resultQuantite = 0;
                     for (let i of array) {
-                        if(optionsProduit.id == i.id)
+                        if(optionsProduit.id == i.id){
                             resultQuantite = i.quantite += optionsProduit.quantite;
-                            resultQuantiteMoins = i.quantite -= optionsProduit.quantite;
-                    }
-                    
+                        }
                         optionsProduit.quantite = resultQuantite;
                         array = array.filter(el => el.id != optionsProduit.id)
-                }else {
-                }                                                                                                                                                            
+                    }  
+                }                                                                                                                                      
                 array.push(optionsProduit);
                 localStorage.setItem("panier", JSON.stringify(array));
                 alert(" Vous avez ajouté l'article " + product.name + " au panier");  
-             }
-             else if([] &&  optionsProduit.colors != "" && optionsProduit.quantite != "0") {
+            }
+            else if(optionsProduit.colors != "" && optionsProduit.quantite != "0") {
                 array = [];
                 array.push(optionsProduit);
                 localStorage.setItem("panier", JSON.stringify(array));
                 alert(" Vous avez ajouté l'article " + product.name + " au panier");
-             }
-             else if(optionsProduit.colors == "" && optionsProduit.quantite != "0") {
-                 alert("Choissisez une couleur pour ajouté votre article au panier");
-             }
-             else if(optionsProduit.quantite = "0" && optionsProduit.colors != "") {
-                 alert("Choissisez la quantité de l'article pour l'ajouté au panier")
-             }
-             else {
-                 alert("L'article n'a pas de couleurs ni de quantité vous ne pouvez pas l'ajouté au panier")
-             }
+            }
+            else if(optionsProduit.colors == "" && optionsProduit.quantite != "0") {
+                alert("Choissisez une couleur pour ajouté votre article au panier");
+            }
+            else if(optionsProduit.quantite = "0" && optionsProduit.colors != "") {
+                alert("Choissisez la quantité de l'article pour l'ajouté au panier")
+            }
+            else {
+                alert("L'article n'a pas de couleurs ni de quantité vous ne pouvez pas l'ajouté au panier")
+            }
 
     })
     //// récupération du localStorage envoyé dans le localStorage dans le console log ////
